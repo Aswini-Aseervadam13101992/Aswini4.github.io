9f6265bbb3dd01620718afe58a6f338de714ba96
Aswini-Aseervadam13101992 <89022488+Aswini-Aseervadam13101992@users.noreply.github.com>
2021-08-19 18:12:02 +0530

    RegExp
    This project is related to Regular Expressions

---
+++ app.js
@@ -0,0 +1,26 @@
+var pattern, check;
+
+
+
+pattern=/Javascript/gi;
+check=pattern.test("Javascript is my favourite");
+console.log(check);
+
+pattern=/@+/g;
+check=pattern.test("Javascript @@ is my favourite");
+console.log(check);
+
+pattern=/\d{1,10000}/g;
+check=pattern.test("12345");   //Add one more digit and see the return value
+console.log(check);
+
+pattern=/^[A-z]/g;
+check=pattern.test("y1abc");   //Change the first char to an alphabet and see the return value
+console.log(check);
+
+pattern=/\w$/g;
+check=pattern.test("1abc_");  //Change the last char to @ and see the return value
+console.log(check);
+
+
+
